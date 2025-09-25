399974de3193531512caa06dde8650e42126d5ff
GabriellyAlcantara <gabrielly.alcantara.moraes@escola.pr.gov.br>
2025-09-25 09:03:20 -0300

    Create aula.java

---
+++ aula.java
@@ -0,0 +1,9 @@
+public class aula{
+    public static void main(String[] args){
+        String nome = "Gaby";
+        int a = 10;
+        int b = 7;
+        System.out.println("Bom dia "+nome+" , sua idade e: "+(a+b)+" anos");
+    }
+}
+
